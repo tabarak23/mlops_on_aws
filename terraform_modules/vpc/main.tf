56af057bee9572db9bb5a3d04a2c1bf4ff4185c0
tabarak23 <er.tabarak2000@gmail.com>
2025-08-13 07:45:48 +0530

    created subnets

--- terraform_modules/vpc/main.tf
+++ terraform_modules/vpc/main.tf
@@ -39,3 +39,50 @@ resource "aws_internet_gateway" "rag_igw" {
     Name = "${local.name}-igw"
   }
 }
+#fetching the names of availaialbe azs
+data "aws_availability_zones" "available"{
+    state= "available"
+}
+
+#now subnets 
+
+#first public subnets
+resource "aws_subnet" "public_subnet" {
+  count= var.az_count
+  vpc_id= aws_vpc.rag_vpc.id
+  cidr_block= cidrsubnet(var.vpc_cidr,8,count.index)
+  availability_zone = data.aws_availability_zones.available[count.index]
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name= "${local.name}-public-subnet-${count.index + 1}"
+  }
+}
+
+
+#private  subnetss
+resource "aws_subnet" "private" {
+  count= var.az_count
+  vpc_id= aws_vpc.main.id
+  cidr_block= cidrsubnet(var.vpc_cidr, 8, count.index + var.az_count)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "${local.name}-private-subnet-${count.index + 1}"
+  }
+}
+
+#database subnets
+
+resource "aws_subnet" "database" {
+  count= var.az_count
+  vpc_id= aws_vpc.main.id
+  cidr_block= cidrsubnet(var.vpc_cidr, 8, count.index + 2 * var.az_count)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "${local.name}-db-subnet-${count.index + 1}"
+  }
+}
+
